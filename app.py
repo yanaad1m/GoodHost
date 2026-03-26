95d48786be18b9dcf43238e0632fe945f81363b6
Violeta44 <vtinc314@gmail.com>
2026-03-26 14:10:11 +0200

    Add volunteer registration, hosts listing, rules routes and templates

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import json
 from flask import Flask, render_template, request
+from app.database import get_db, init_db
 
 app = Flask(__name__)
+
+
+@app.template_filter('from_json')
+def from_json_filter(value):
+    if value:
+        try:
+            return json.loads(value)
+        except Exception:
+            return []
+    return []
 
 
 @app.route('/')
@@ -21,5 +33,34 @@ def hostsregistration():
     return render_template('hostsregistration.html')
 
 
+@app.route('/register/volunteer', methods=['GET', 'POST'])
+def volunteer_registration():
+    if request.method == 'POST':
+        # TODO: обработка на данните от формата
+        pass
+    return render_template('volunteer_registration.html')
+
+
+@app.route('/hosts')
+def hosts():
+    search = request.args.get('search', '').strip()
+    db = get_db()
+    if search:
+        hosts_list = db.execute(
+            'SELECT * FROM hosts WHERE name LIKE ? OR location LIKE ? ORDER BY created_at DESC',
+            (f'%{search}%', f'%{search}%')
+        ).fetchall()
+    else:
+        hosts_list = db.execute('SELECT * FROM hosts ORDER BY created_at DESC').fetchall()
+    db.close()
+    return render_template('hosts.html', hosts=hosts_list, search=search)
+
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
